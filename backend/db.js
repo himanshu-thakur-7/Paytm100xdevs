17be0802fb154c8e16340e01ab9af8611be86457
Himanshu Thakur <61612477+himanshu-thakur-7@users.noreply.github.com>
2024-04-13 01:54:47 +0530

    Added authorization routes

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,37 @@
 const mongoose = require("mongoose")
 // create schema for users
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    firstName: String,
-    lastName: String
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
 });
 
 // create model from schema
-const user = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = {
-    user
+    User
 };
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const rootRouter = require("./routes/index")
+const cors = require("cors");
+
 const app = express();
-const PORT = process.env.PORT | 8080;
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT | 3000;
+
+
+app.use('/api/v1', rootRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on Port ${PORT}`)
