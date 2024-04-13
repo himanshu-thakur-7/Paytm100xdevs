39c4472d530191b4b1edc42464e742ec3d6c81be
Himanshu Thakur <61612477+himanshu-thakur-7@users.noreply.github.com>
2024-04-13 15:33:38 +0530

    Added update user endpoint

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,7 @@ const zod = require("zod");
 const { User } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
+const { authMiddleware } = require("../middleware");
 
 const router = express.Router();
 
@@ -81,6 +82,27 @@ router.post("signin", async (req, res) => {
     })
 
 })
+
+const updateBody = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+})
+
+router.put("/", authMiddleware, async (req, res) => {
+    const { success } = updateBody.safeParse(req.body);
+    if (!success) {
+        res.status(411).json({
+            message: "Error while updating information",
+
+        })
+    }
+    await User.updateOne({ _id: req.userId }, req.body);
+    res.json({
+        message: "Updated successfully"
+    })
+})
+
 
 
 module.exports = router;
